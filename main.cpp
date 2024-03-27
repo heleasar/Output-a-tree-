212ec865c2619764ac8b38dead7b031b6bc40de8
Hugo Eleasar Benavides <86984765+heleasar@users.noreply.github.com>
2024-03-27 18:28:37 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Output the tree
+    std::cout << "   *" << std::endl;
+    std::cout << "  ***" << std::endl;
+    std::cout << " *****" << std::endl;
+    std::cout << "*******" << std::endl;
+    std::cout << "  ***" << std::endl;
+
+    // Output two blank lines
+    std::cout << std::endl << std::endl;
+
+    // Output the cat
+    std::cout << "/\\   /\\" << std::endl;
+    std::cout << "  o o" << std::endl;
+    std::cout << " =   =" << std::endl;
+    std::cout << "  ---" << std::endl;
+
+    return 0;
+}
